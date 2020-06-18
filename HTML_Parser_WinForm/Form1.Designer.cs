2ee16d3be4ab1df4706069d6699dfcfbfe30884e
LuckyMakson <40035210+LuckyMakson@users.noreply.github.com>
2020-06-18 11:34:00 +0300

    Add files via upload

---
+++ HTML_Parser_WinForm/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace HTML_Parser_WinForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Back = new System.Windows.Forms.Button();
+            this.DataView = new System.Windows.Forms.DataGridView();
+            this.ShowProducts = new System.Windows.Forms.Button();
+            this.Next = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.DataView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Back
+            // 
+            this.Back.Location = new System.Drawing.Point(590, 418);
+            this.Back.Name = "Back";
+            this.Back.Size = new System.Drawing.Size(71, 20);
+            this.Back.TabIndex = 2;
+            this.Back.Text = "Back";
+            this.Back.UseVisualStyleBackColor = true;
+            this.Back.Click += new System.EventHandler(this.ToMain_Click);
+            // 
+            // DataView
+            // 
+            this.DataView.AllowUserToDeleteRows = false;
+            this.DataView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DataView.Location = new System.Drawing.Point(12, 12);
+            this.DataView.Name = "DataView";
+            this.DataView.ReadOnly = true;
+            this.DataView.Size = new System.Drawing.Size(960, 400);
+            this.DataView.TabIndex = 7;
+            // 
+            // ShowProducts
+            // 
+            this.ShowProducts.Location = new System.Drawing.Point(744, 418);
+            this.ShowProducts.Name = "ShowProducts";
+            this.ShowProducts.Size = new System.Drawing.Size(105, 20);
+            this.ShowProducts.TabIndex = 8;
+            this.ShowProducts.Text = "Show products";
+            this.ShowProducts.UseVisualStyleBackColor = true;
+            this.ShowProducts.Click += new System.EventHandler(this.GetProducts_Click);
+            // 
+            // Next
+            // 
+            this.Next.Location = new System.Drawing.Point(667, 418);
+            this.Next.Name = "Next";
+            this.Next.Size = new System.Drawing.Size(71, 20);
+            this.Next.TabIndex = 9;
+            this.Next.Text = "Next";
+            this.Next.UseVisualStyleBackColor = true;
+            this.Next.Click += new System.EventHandler(this.Next_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 450);
+            this.Controls.Add(this.Next);
+            this.Controls.Add(this.ShowProducts);
+            this.Controls.Add(this.DataView);
+            this.Controls.Add(this.Back);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.DataView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button Back;
+        private System.Windows.Forms.DataGridView DataView;
+        private System.Windows.Forms.Button ShowProducts;
+        private System.Windows.Forms.Button Next;
+    }
+}
+
---
+++ HTML_Parser_WinForm/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using HtmlAgilityPack;
+using System.Net;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Linq;
+
+namespace HTML_Parser_WinForm
+{
+    public partial class Form1 : Form
+    {
+        ScraperCategories scraper;
+        ScraperSubCategories subscraper;
+        ScraperProducts products_scraper;
+
+        int rowIndex;
+
+        List<EntryModel> categories = new List<EntryModel>();
+        List<ProductModel> subcategories = new List<ProductModel>();
+        List<ProductModel> sublist = new List<ProductModel>();
+        List<EntryModel> products = new List<EntryModel>();
+
+        List<DataAllo> data = new List<DataAllo>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            scraper = new ScraperCategories();
+            subscraper = new ScraperSubCategories();
+            products_scraper = new ScraperProducts();
+            categories.Clear();
+
+            ShowProducts.Enabled = false;
+            ShowProducts.Visible = false;
+
+            ShowProducts.Enabled = false;
+            ShowProducts.Visible = false;
+
+            Back.Enabled = false;
+            Back.Visible = false;
+
+            categories = scraper.ScrapeData("https://allo.ua/ru/");
+
+            ShowData(categories);
+            foreach (var item in categories)
+            {
+                data.Add(new DataAllo { Title = item.Title, Url = item.Url });
+            }
+            return;
+        }
+
+        private void ToMain_Click(object sender, EventArgs e)
+        {
+            ShowData(categories);
+            ShowProducts.Enabled = false;
+            ShowProducts.Visible = false;
+            Next.Enabled = true;
+            Next.Visible = true;
+            subcategories.Clear();
+            products.Clear();
+            rowIndex = 0;
+            Back.Enabled = false;
+            Back.Visible = false;
+        }
+
+        private void GetProducts_Click(object sender, EventArgs e)
+        {
+            if (subscraper.CheckSubCategories(data[rowIndex].subcategories[DataView.CurrentCell.RowIndex].Url))
+            {
+                // subcategories = data[rowIndex].subcategories;
+                data[rowIndex].subcategories.AddRange(subscraper.ScrapeData(data[rowIndex].subcategories[DataView.CurrentCell.RowIndex].Url));
+
+                //  data[rowIndex].subcategories.RemoveAt(DataView.CurrentCell.RowIndex);
+
+                // data[rowIndex].subcategories = subcategories;
+
+                // ShowDataProd(data[rowIndex].subcategories);
+                sublist = subcategories;
+                ShowDataProd(sublist);
+
+            }
+            else
+            {
+                products = products_scraper.ScrapeData(data[rowIndex].subcategories[DataView.CurrentCell.RowIndex].Url);
+                //data[rowIndex].subcategories[DataView.CurrentCell.RowIndex].products = products;
+
+                ShowData(products);
+
+                ShowProducts.Enabled = false;
+                ShowProducts.Visible = false;
+            }
+        }
+
+        public void ShowDataProd(List<ProductModel> list)
+        {
+            DataView.Columns.Clear();
+            DataView.DataSource = list;
+            DataView.Refresh();
+            DataView.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+        }
+
+        public void ShowData(List<EntryModel> list)
+        {
+            DataView.Columns.Clear();
+            DataView.DataSource = list;
+            DataView.Refresh();
+            DataView.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+        }
+
+        private void Next_Click(object sender, EventArgs e)
+        {
+            Back.Enabled = true;
+            Back.Visible = true;
+            rowIndex = DataView.CurrentCell.RowIndex;
+
+            DataView.Columns.Clear();
+            subcategories = subscraper.ScrapeData(categories[rowIndex].Url);
+
+            if (subcategories.Count != 0)
+            {
+                data[rowIndex].subcategories = subcategories;
+                ShowDataProd(data[rowIndex].subcategories);
+                //DataView.DataSource = data[rowIndex].subcategories;
+
+                ShowProducts.Visible = true;
+                ShowProducts.Enabled = true;
+            }
+
+            if (subcategories.Count == 0)
+                ShowData(categories);
+
+            Next.Enabled = false;
+            Next.Visible = false;
+        }
+    }
+}
